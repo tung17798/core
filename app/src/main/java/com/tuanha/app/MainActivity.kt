e4bbbbd34b42fd5e22842d10ca471ea9a8367984
Tung Tran <tranngoctung17798@gmail.com>
2024-09-30 21:50:34 +0700

    push

--- app/src/main/java/com/tuanha/app/MainActivity.kt
+++ app/src/main/java/com/tuanha/app/MainActivity.kt
@@ -2,8 +2,10 @@ package com.tuanha.app
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.simple.coreapp.ui.base.activities.BaseViewBindingActivity
+import com.tuanha.app.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseViewBindingActivity<ActivityMainBinding>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
